be4563001b8bbb9e589cbd88d7ee96b92713cde5
peauli <preciousajorgba@gmail.com>
2024-04-15 13:41:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const binanceWS = Binance().ws;
 // Symbol and interval for which you want to receive WebSocket Kline data
 const symbol = 'BTCUSDT';
 const interval = '1m'; // 1 minute interval
-const PORT = process.env.PORT || 3000;
-const wss = new WebSocket.Server({ port: PORT })
+const wss = new WebSocket.Server({ port: 3000 });
+
 
 // Subscribe to the WebSocket stream for Kline data
 binanceWS.candles(symbol, interval, (candle) => {
